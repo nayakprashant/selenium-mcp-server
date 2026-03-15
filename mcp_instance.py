1e7d231ffba45ef98e43cfeacbc34e194875b66b
Prashant Nayak <prashantnayak@Prashants-MacBook-Pro.local>
2026-03-15 15:54:55 +0530

    major package  restructing, updated readme, deleted unwanted files, enhanced logger

--- mcp_instance.py
+++
@@ -1,8 +0,0 @@
-from mcp.server.fastmcp import FastMCP
-from utils.logger import logger
-
-logger.info("Initializing Selenium MCP Server...")
-
-mcp = FastMCP(
-    "selenium-mcp-server"
-)
--- tools/page_tools.py
+++ tools/page_tools.py
@@ -1,4 +1,4 @@
-from mcp_instance import mcp
+from instance.mcp_instance import mcp
 from core.session_manager import *
 from selenium.webdriver.common.by import By
 from utils.logger import logger
